f98dfd695e26d4c0aec6259200a149ea79f7cde5
taekyeo110 <tkyeo@artience.com>
2021-08-26 16:47:08 +0900

    INIT

---
+++ src/test/java/KafkaSpring/demo/JavaPracticeTests/Practice1Tests.java
@@ -0,0 +1,14 @@
+package KafkaSpring.demo.JavaPracticeTests;
+
+public class Practice1Tests {
+
+//    @DisplayName("자바 테스트 1")
+//    @Test
+    void mainTest() {
+        // given
+
+        // when
+
+        // then
+    }
+}
---
+++ src/test/java/KafkaSpring/demo/Kafka/KafkaConfigurationTests.java
@@ -0,0 +1,4 @@
+package KafkaSpring.demo.Kafka;
+
+public class KafkaConfigurationTests {
+}
